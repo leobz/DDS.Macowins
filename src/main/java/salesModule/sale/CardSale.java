0ad493bb5161e9cef3ec90c5376a86718431febb
Leonel Bazan <leo.bazan.x@gmail.com>
2020-04-03 15:44:45 -0300

    COEFFICIENT attribute of CardSale class is now a constant.
    It's a requirement of the client that the coefficient must be
    constant

--- src/main/java/salesModule/sale/CardSale.java
+++ src/main/java/salesModule/sale/CardSale.java
@@ -7,23 +7,23 @@ import salesModule.common.CreditCard;
 import salesModule.item.ClothingItem;
 
 public class CardSale extends Sale {
-	double coefficient = 10;
+	private static final double COEFFICIENT = 10;
 	int fees;
 	CreditCard card;
-	
+
 	public CardSale(List<ClothingItem> items, Date date, CreditCard card, int fees) {
 		super(items, date);
 		this.card = card;
 		this.fees = fees;
 	}
-	
+
 	public double value() {
 		double price = super.value();
-		double surcharge = this.fees * coefficient + itemPricePercentageSum(0.01);
+		double surcharge = this.fees * COEFFICIENT + itemPricePercentageSum(0.01);
 		return price + surcharge;
 	}
-	
-	public double itemPricePercentageSum(double coefficient) {
-		return items.stream().mapToDouble(item -> item.price() * coefficient).sum();
+
+	public double itemPricePercentageSum(double COEFFICIENT) {
+		return items.stream().mapToDouble(item -> item.price() * COEFFICIENT).sum();
 	}
 }
